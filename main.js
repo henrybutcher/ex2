fc83dad49f4c84cd7f0f0b73185b14547ccf2353
henrybutcher <butcher301355@gamil.com>
2023-06-03 11:25:38 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const me = {
+    firstName: 'Henry',
+    lastName: 'Butcher',
+    age: 20
+}
+
+const oddNumber = [1, 3, 5, 7]
+
+console.log(me)
+
+console.log('My brothers age is')
+
+console.log(oddNumber [0]*19)
